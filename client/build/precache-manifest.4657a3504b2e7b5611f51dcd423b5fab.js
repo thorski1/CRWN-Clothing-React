15ec06d22f4547f8a3f88a53725cf8fc0b8a77db
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-08-31 22:51:51 -0400

    no clue what Im doing

---
+++ client/build/precache-manifest.4657a3504b2e7b5611f51dcd423b5fab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a12980f71e23e1d3c6b17834c91e081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd13007f8112f6b28eae",
+    "url": "/static/css/main.6410950d.chunk.css"
+  },
+  {
+    "revision": "fdf23f4be83faa79140f",
+    "url": "/static/js/2.3b2d5560.chunk.js"
+  },
+  {
+    "revision": "bd13007f8112f6b28eae",
+    "url": "/static/js/main.62157a92.chunk.js"
+  },
+  {
+    "revision": "b721ef581ceece9d2d8b",
+    "url": "/static/js/runtime~main.43ea9386.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
