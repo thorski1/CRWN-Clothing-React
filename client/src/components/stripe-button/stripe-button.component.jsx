5769c7af11120b85240504c1b8648a0b2c428506
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-08-31 22:02:23 -0400

    setup for heroku, backend for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
-
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_SXMHLJ4Co9oi9DXIRkPABU8b00bXnJPVHG';
-  const onToken = token => {
-    alert('Payment successful!');
-  };
-  return (
-    <StripeCheckout
-      label="Pay Now"
-      name="CRWN Clothing Ltd."
-      billingAddress
-      shippingAddress
-      image="http://www.innalabs.com/wp-content/uploads/2017/08/004-credit-card.svg"
-      description={`Your total is ${price}`}
-      amount={priceForStripe}
-      panelLabel="Pay Now"
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+	const priceForStripe = price * 100;
+	const publishableKey =
+		"pk_test_SXMHLJ4Co9oi9DXIRkPABU8b00bXnJPVHG";
+	const onToken = token => {
+		axios({
+			url: "payment",
+			method: "post",
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		})
+			.then(response => {
+				alert("Payment Successful");
+			})
+			.catch(error => {
+				console.log("Payment error: " + JSON.parse(error));
+				alert(
+					"There was an issue with your payment. Please make sure you use the correct credit card information provided."
+				);
+			});
+	};
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="http://www.innalabs.com/wp-content/uploads/2017/08/004-credit-card.svg"
+			description={`Your total is ${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
 };
 
 export default StripeCheckoutButton;
