dcd771de65439e2f9e5ec9060df1d3d5548eef25
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-09-02 18:19:37 -0400

    add service worker and compression

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const compression = require("compression");
+const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production")
 	require("dotenv").config();
@@ -13,12 +15,15 @@ const stripe = require("stripe")(
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+	app.use(enforce.HTTPS({ trustProtoHeader: true }));
 	app.use(
 		express.static(path.join(__dirname, "client/build"))
 	);
@@ -33,6 +38,17 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
 	if (error) throw error;
 	console.log("Server running on port " + port);
+});
+
+app.get("/service-worker.js", (req, res) => {
+	res.sendFile(
+		path.resolve(
+			__dirname,
+			"..",
+			"build",
+			"service-worker.js"
+		)
+	);
 });
 
 app.post("/payment", (req, res) => {
